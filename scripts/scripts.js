a55474fec2857adf36e3818d41da0b08af71e47b
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-21 10:30:31 -0700

    js file
    selected all HMTL elements and event listener added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,20 @@
-console.log("Ganesh, Krushna");
+let initialPrice = document.querySelector("#initialPrice");
+let quantity = document.querySelector("#quantity");
+let currentPrice = document.querySelector("#currentPrice");
+let tellMe = document.querySelector("#btn-tell");
+let displayOutput = document.querySelector("#displayOutput");
+
+
+
+function clickHandler() {
+     console.log(initialPrice.value + " quantity " + quantity.value + " currentPrice is : " + currentPrice.value);
+
+}
+
+
+
+
+
+
+
+tellMe.addEventListener("click", clickHandler)
