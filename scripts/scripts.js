fb81f0b9c9830d1c4ec4619992e3377801ff933f
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-21 13:31:39 -0700

    update js
    inputs captured in variables

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,13 +5,25 @@ let tellMe = document.querySelector("#btn-tell");
 let displayOutput = document.querySelector("#displayOutput");
 
 
+
+
+
+
+
+// initialPrice= initialPrice.value; 
+// quantity = quantity.value;
+// currentPrice = currentPrice.value; 
+
+// console.log("given inputs are " + initialPrice.value + quantity.value + currentPrice.value);
+
+
 // function extractValues(input1, input2, input3) {
 //     let input1  = input1.value ;
 //     let input2 = input2.value;
 //     let input3 = input3.value ; 
 //     console.log (input1, input2, input3); 
 //     return (input1, input2, input3);
-   
+
 // } 
 // extractValues(initialPrice, quantity, currentPrice);
 
@@ -26,53 +38,56 @@ let displayOutput = document.querySelector("#displayOutput");
 
 
 
-function calculateProfitOrLoss(initialPrice, currentPrice, quantity)  {
-//  console.log("hello " + initialPrice + " " + currentPrice + " " + quantity); 
-  
-    
+function calculateProfitOrLoss(initialPrice, currentPrice, quantity) {
 
     let costPrice = initialPrice * quantity;
-    let sellingPrice = currentPrice * quantity; 
+    let sellingPrice = currentPrice * quantity;
 
     if (sellingPrice > costPrice) {
-        let profit = sellingPrice - costPrice;       
-        let profitPercent = (profit/costPrice)*100; 
-         profitPercent = profitPercent.toFixed(2);
+        let profit = sellingPrice - costPrice;
+        let profitPercent = (profit / costPrice) * 100;
+        profitPercent = profitPercent.toFixed(2);
 
         // console.log(profit + " is profit made and percentage profit is " + profitPercent );
 
-        displayOutput.style.color = "green"; 
-        displayOutput.innerText = `Yay! You made  a profit of  ${profit} & profit percentage is ${profitPercent}`; 
+        displayOutput.style.color = "green";
+        displayOutput.innerText = `Yay! You made  a profit of  ${profit} & profit percentage is ${profitPercent}%`;
 
     } else if (sellingPrice < costPrice) {
         let loss = costPrice - sellingPrice;
-        let lossPercent = (loss/costPrice)*100; 
-        lossPercent = lossPercent.toFixed(2); 
+        let lossPercent = (loss / costPrice) * 100;
+        lossPercent = lossPercent.toFixed(2);
 
         // console.log("Aww... Loss is " + loss + ". loss percentage is " + lossPercent); 
 
-        displayOutput.style.color="red"; 
-        displayOutput.innerText = `Awww... Loss is ${loss} & loss percentage is ${lossPercent} ` ;
+        displayOutput.style.color = "red";
+        displayOutput.innerText = `Awww... Loss is ${loss} & loss percentage is ${lossPercent}% `;
 
-    }   else if (sellingPrice = costPrice) {
+    } else if (sellingPrice = costPrice) {
 
         displayOutput.innerText = `Break-even! No profit, No loss`;
-        displayOutput.style.color="blue"; 
-    }   else {
-        displayOutput.style.color="firebrick"; 
-        displayOutput.innerText= `Please enter valid numbers in all the input fields.`
-    }      
+        displayOutput.style.color = "blue";
+    } else {
+        displayOutput.style.color = "firebrick";
+        displayOutput.innerText = `Please enter valid numbers in all the input fields.`
+    }
 }
 
- 
 
-function clickHandler() {  
-    if (initialPrice.value <= 0 || currentPrice.value <=0 || quantity.value <= 0) {
+
+function clickHandler() {
+    // trying.innerText = "H2 is " + iniPri  + ", " + qty + ", " + currPri ;
+
+    let iniPri = initialPrice.value;
+    let qty = quantity.value;
+    let currPri= currentPrice.value; 
+
+    if (iniPri <= 0 || currPri <= 0 || qty <= 0) {
         displayOutput.innerText = "Stock Trading at 0 is not possible. Please verify the numbers again!";
         console.log("you are tricking me. negatives added")
     } else {
-        console.log("no negatives added"); 
-        calculateProfitOrLoss(initialPrice.value, currentPrice.value, quantity.value); 
+        console.log("no negatives added");
+        calculateProfitOrLoss(iniPri, currPri, qty);
     }
 
 
